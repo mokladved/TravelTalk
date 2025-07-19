bf0fb3ff4259bd19256b648c7cd1baa9b3503b86
moklad <dev.moklad@gmail.com>
2025-07-19 23:42:22 +0900

    feat(View): 컴포넌트별 UI 설정

--- TravelTalk/TravelCollectionViewCell.swift
+++ TravelTalk/TravelCollectionViewCell.swift
@@ -8,15 +8,52 @@
 import UIKit
 
 class TravelCollectionViewCell: UICollectionViewCell {
-    
     static let identifier = "TravelCollectionViewCell"
 
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var messageLabel: UILabel!
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         configureUI()
     }
     
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        configureProfileImageVeiwUI()
+    }
+    
+    
     func configureUI() {
-        backgroundColor = .red
+        configureNameLabelUI()
+        configureDateLabelUI()
+        configureMessasgeLabelUI()
+    }
+    
+    
+    func configureNameLabelUI() {
+        nameLabel.font = .boldSystemFont(ofSize: 13)
+    }
+    
+    func configureMessasgeLabelUI() {
+        messageLabel.font = .systemFont(ofSize: 13)
+        messageLabel.textColor = UIColor.grayForegroundColor
+        
+    }
+    
+    
+    func configureProfileImageVeiwUI() {
+        profileImageView.layer.cornerRadius = profileImageView.frame.width / 2
+        profileImageView.clipsToBounds = true
+    }
+    
+    func configureDateLabelUI() {
+        dateLabel.textColor = UIColor.grayForegroundColor
+        dateLabel.font = .systemFont(ofSize: 13)
     }
 }
