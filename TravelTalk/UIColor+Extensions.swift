28b17175843a8336b1fd906e11c7851735a098ea
moklad <dev.moklad@gmail.com>
2025-07-19 23:35:10 +0900

    feat(style): 커스텀 색상 추가

---
+++ TravelTalk/UIColor+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Extensions.swift
+//  TravelTalk
+//
+//  Created by Youngjun Kim on 7/19/25.
+//
+
+import UIKit
+
+extension UIColor {
+    static let grayBackgroundColor = UIColor(red: 231/255.0, green: 231/255.0, blue: 231/255.0, alpha: 1.0)
+    static let grayForegroundColor = UIColor(red: 170/255.0, green: 170/255.0, blue: 170/255.0, alpha: 1.0)
+}
