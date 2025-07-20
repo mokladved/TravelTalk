f71e6276f55df6360bb5af70fc80a531e7a233eb
moklad <dev.moklad@gmail.com>
2025-07-21 03:36:31 +0900

    feat(Search): 친구 이름으로 검색하는 로직 구현

--- TravelTalk/TravelViewController.swift
+++ TravelTalk/TravelViewController.swift
@@ -10,6 +10,7 @@
     final class TravelViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
         
         private let chatLists = ChatList.list
+        private var filteredChatLists: [ChatRoom] = []
         
         @IBOutlet var viewWrappedTextField: UIView!
         
@@ -22,6 +23,8 @@
         
         override func viewDidLoad() {
             super.viewDidLoad()
+            filteredChatLists = chatLists
+            
             registerCell()
             travelCollectionView.delegate = self
             travelCollectionView.dataSource = self
@@ -32,25 +35,25 @@
             configureSearchTextFieldUI()
             configureImageWrappedTextFieldUI()
             configureBackButtonTitle()
+            
         }
         
         func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            return chatLists.count
+            return filteredChatLists.count
         }
         
         func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
             let cell = travelCollectionView.dequeueReusableCell(withReuseIdentifier: TravelCollectionViewCell.identifier, for: indexPath) as! TravelCollectionViewCell
             
-            let chat = chatLists[indexPath.item]
-            let lastChat = chat.chatList[chat.chatList.count-1]
-            let chatroomImage = chat.chatroomImage
-            let lastChatUser = lastChat.user.name
-            let lastChatMessage = lastChat.message
-            let lastChatDate = lastChat.formattedDate
-            
-            cell.configureData(imageName: chatroomImage, name: lastChatUser, message: lastChatMessage, date: lastChatDate)
-            
-            
+            let chat = filteredChatLists[indexPath.item]
+            if let lastChat = chat.chatList.last {
+                let chatroomImage = chat.chatroomImage
+                let lastChatUser = lastChat.user.name
+                let lastChatMessage = lastChat.message
+                let lastChatDate = lastChat.formattedDate
+                        
+                cell.configureData(imageName: chatroomImage, name: lastChatUser, message: lastChatMessage, date: lastChatDate)
+            }
             return cell
         }
         
@@ -91,6 +94,25 @@
             )
             searchTextField.backgroundColor =  .clear
             searchTextField.borderStyle = . none
+            
+            searchTextField.addTarget(self, action: #selector(searchTextFieldChanged), for: .editingChanged)
+        }
+        
+        @objc private func searchTextFieldChanged() {
+            guard let searchText = searchTextField.text else {
+                return
+            }
+            
+            if searchText.trimmingCharacters(in: .whitespaces).isEmpty {
+                filteredChatLists = chatLists
+            } else {
+                filteredChatLists = chatLists.filter { room in
+                    return room.chatList.contains { chat in
+                        return chat.user != ChatList.me && chat.user.name.lowercased().contains(searchText)
+                    }
+                }
+            }
+            travelCollectionView.reloadData()
         }
         
         private func configureSearchButtonUI() {
@@ -105,7 +127,7 @@
         }
         
         func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-            let chatRoom = ChatList.list[indexPath.item]
+            let chatRoom = filteredChatLists[indexPath.item]
                 
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
             let chatVC = storyboard.instantiateViewController(withIdentifier: ChatViewController.identifier) as! ChatViewController
